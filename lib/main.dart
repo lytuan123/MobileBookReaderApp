5212635f9b50295e5bbe3f3e487c7b6b6e242e02
lytuan123 <130950679+lytuan123@users.noreply.github.com>
2024-11-14 10:39:55 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,51 +18,14 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
-  final List<String> _bookFiles = ['sample1.prc', 'sample2.prc']; // Thay thế với tên file thực tế
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Book Reader'),
       ),
-      body: ListView.builder(
-        itemCount: _bookFiles.length,
-        itemBuilder: (context, index) {
-          String fileName = _bookFiles[index];
-          return ListTile(
-            title: Text(fileName),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => BookReaderScreen(fileName: fileName),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class BookReaderScreen extends StatelessWidget {
-  final String fileName;
-
-  BookReaderScreen({required this.fileName});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Reading $fileName'),
-      ),
       body: Center(
-        child: Text(
-          'Content of $fileName will be displayed here',
-          style: TextStyle(fontSize: 18),
-        ),
+        child: Text('Welcome to the Book Reader App!'),
       ),
     );
   }
